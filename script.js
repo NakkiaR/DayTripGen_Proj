a5397caaeb75ec23ec218b7a5e601f41d090612d
Vernon Roberts <nakkiasr@live.com>
2021-08-01 19:41:04 -0700

    Final result. Was unable to get the loops to work properly.

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function welcomeMessage() {
     message = "Welcome!";
 }   
 let userName = prompt("Welcome to the online day trip planner. Please enter your first name.");
-alert ("Hello " + userName + "!" + " Let's get started with planning your day trip!");
+alert ("Hi " + userName + "!" + " Let's get started with planning your day trip!");
 
 //Destination
 function getDayTrip() {
@@ -14,18 +14,16 @@ function getDayTrip() {
     return dayTrip;
 }
 let myDayTrip = ["Sedona", "Flagstaff", "Phoenix"];
+//for(let i = 0; i < 3; i++) {
 let dayTripDestination = myDayTrip[Math.floor(Math.random() * myDayTrip.length)];
 alert ("City: " + dayTripDestination);
-// let userInput = prompt("Would you like to keep this destination? Type, 'yes' or 'no' below.");
-// if(userInput == "yes") {
-//     alert ("Great! Next-Let's grab your transportation!");
-console.log("You are going to " + dayTripDestination);
-
-// else {
-// alert ("Okay, No worries! Please refresh the page to grab another option.");
-
-// }
-//
+//let userInput = prompt("Would you like to go to this city? Type, 'yes' or 'no' below.");
+//if(userInput == "yes") {
+    //alert ("Great! Next-Let's grab your transportation!");
+    console.log("You are going to " + dayTripDestination);
+    //}
+//else if(userInput == "no") {
+//}
 //}
 
 //Transportation
@@ -33,19 +31,19 @@ function getTripTransportation() {
     let tripTransportation = "";
     return tripTransportation;
 }
-let myTripTransportation = ["car", "bus", "Uber"];
+let myTripTransportation = ["personal car", "party bus", "Uber"];
+//for(let i = 0; i < 3; i++) {
 let tripTransportation = myTripTransportation[Math.floor(Math.random() * myTripTransportation.length)];
 alert ("Mode of transportation: " + tripTransportation);
-// userInput = prompt("Would you like to keep this mode of transportation? Type, 'yes' or 'no' below.");
-// if(userInput == "yes") {
-//     alert ("Fantastic! Next-Let's find some food!");
-console.log("Your mode of transportation will be by " + tripTransportation);
+//let userInput = prompt("Would you like to keep this mode of transportation? Type, 'yes' or 'no' below.");
+//if(userInput == "yes") {
+    //alert ("Fantastic! Next-Let's find some food!");
+    console.log("Your mode of transportation will be by " + tripTransportation);
+    //break;
 // }
-// else {
-//     alert ("Okay, no problem! Hit that refresh button to grab a different mode!")
+// else if(userInput == "no") {
 // }
-//
-//}
+// }
 
 //Restaurant
 function getTripRestaurant() {
@@ -54,10 +52,19 @@ return tripRestaurant;
 }
 if(dayTripDestination == "Sedona") {
     let myRestaurantSedona = ["Casa Sedona Restaurant", "Mesa Grill Sedona", "Creekside American Bistro"];
+    //for(let i = 0; i < 3; i++) {
     let tripRestaurant;
     tripRestaurant = myRestaurantSedona[Math.floor(Math.random() * myRestaurantSedona.length)];
     alert ("Restaurant: " + tripRestaurant);
-    console.log("Your place of grub will be" + tripRestaurant);
+    //      userInput = prompt("Does this sound good? Type, 'yes' or 'no' below.");
+    // if(userInput == "yes") {
+    //     alert ("Great choice! Finally-Let's find some entertainment!");
+        console.log("Your place of grub will be" + tripRestaurant);
+//     break;
+// }
+// else if(userInput == "no") {
+// }
+// }
 }
     else if(dayTripDestination === "Flagstaff") {
         let myRestaurantFlagstaff = ["Big Canyon BBQ ", "MartAnnes Burrito Palace", "Fat Olives"];
@@ -73,15 +80,6 @@ if(dayTripDestination == "Sedona") {
         alert ("Restaurant: " + tripRestaurant);
     console.log("Your place of yum will be " + tripRestaurant);
 }
-//     userInput = prompt("Does this sound good? Type, 'yes' or 'no' below.");
-//     if(userInput == "yes") {
-//         alert ("Great choice! Finally-Let's find some entertainment!");
-// }
-//     else {
-//         alert ("Sure thing! Click refresh to find a different restaurant!");
-        
-//}
-//}
 
 //Entertainment
 function getTripEntertainment() {
@@ -93,30 +91,31 @@ if(dayTripDestination === "Sedona") {
     let tripEntertainment;
     tripEntertainment = myEntertainmentSedona[Math.floor(Math.random() * myEntertainmentSedona.length)];
     alert ("Entertainment: " + tripEntertainment);
-console.log("Have a wonderful time at " + tripEntertainment);
+console.log("Your place of entertainment is " + tripEntertainment);
 }
 else if(dayTripDestination === "Flagstaff") {
     let myEntertainmentFlagstaff = ["Escape Rooms Flagstaff", "Starlite Lanes", "Orpheum Theater"];
     let tripEntertainment;
     tripEntertainment = myEntertainmentFlagstaff[Math.floor(Math.random() * myEntertainmentFlagstaff.length)];
     alert ("Entertainment: " + tripEntertainment);
-console.log("Have fun at " + tripEntertainment);
+console.log("Your place of entertainment is " + tripEntertainment);
 }
 else if(dayTripDestination === "Phoenix") {
     let myEntertainmentPhoenix = ["FilmBar", "Copper Blues Rock Pub & Kitchen", "Cobra Arcade Bar"];
     let tripEntertainment;
     tripEntertainment = myEntertainmentPhoenix[Math.floor(Math.random() * myEntertainmentPhoenix.length)];
     alert ("Entertainment: " + tripEntertainment);
-console.log("Have a great time at " + tripEntertainment);
+console.log("Your place of entertainment is " + tripEntertainment);
 }
 
 //Confirmation
 //function confirmTrip() {
 let userInput = prompt("Review the selections below. Would you like to keep this day trip? Type, 'yes' or 'no' below.");
 if(userInput == "yes") {
-alert ("Great! Have a wonderful time!");
+alert ("Great! Enjoy your day trip!");
 }
 else {
-alert ("Okay, No worries! Please refresh the page to start over.");
+        alert ("Okay, No worries! Just hit that refresh icon to start over.");
 }
+//}
 //}
